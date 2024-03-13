b0dc5eb5660ee3e446ea83bbf89d8b8aec6214c8
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2024-03-13 12:13:42 -0300

    ajustado app.config

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import { IonicRouteStrategy, IonicModule } from '@ionic/angular';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { MandamentosService } from './app/mandamentos/mandamentos.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    importProvidersFrom(IonicModule.forRoot({})),
+    MandamentosService,
+  ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,11 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import {
-  IonicRouteStrategy,
-  provideIonicAngular,
-} from '@ionic/angular/standalone';
-
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { IonicModule } from '@ionic/angular';
-import { MandamentosService } from './app/mandamentos/mandamentos.service';
+import { appConfig } from './app-config';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    importProvidersFrom(IonicModule.forRoot({})),
-    MandamentosService,
-  ],
-});
+bootstrapApplication(AppComponent, appConfig);
